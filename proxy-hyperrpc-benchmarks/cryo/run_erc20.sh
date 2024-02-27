6cc5ec77a068ced814a12a460fdc983a617b3ceb
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2024-02-27 17:22:12 +0600

    bench(cryo): use inner-request-size param

--- proxy-hyperrpc-benchmarks/cryo/run_erc20.sh
+++ proxy-hyperrpc-benchmarks/cryo/run_erc20.sh
@@ -1,2 +1,2 @@
-cryo erc20_transfers -b 18753440:18757440 --chunk-size 1000 --rpc "http://localhost:3113" -o data --hex
+cryo erc20_transfers -b 18753440:18757440 --chunk-size 1000 --rpc "http://localhost:3113" -o data --hex --inner-request-size 50
 rm -rf data
